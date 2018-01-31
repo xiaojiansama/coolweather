227f52b3e83a300b5e21f3d693fea15668c8375b
Your name <827616556@qq.com>
2018-01-31 16:52:11 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/sama/coolweather/db/City.java
@@ -0,0 +1,50 @@
+package com.example.sama.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 小贱`sama on 2018/1/31.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private  String cityName;
+
+    private  int cityCode;
+
+    private int provinceId;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getcityName(){
+        return cityName;
+    }
+
+    public void setcityName(String cityName){
+        this.cityName = cityName;
+    }
+
+    public int getcityCode(){
+        return cityCode;
+    }
+
+    public void setProvinceCode(int cityCode){
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId(){
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId){
+        this.provinceId = provinceId;
+    }
+}
